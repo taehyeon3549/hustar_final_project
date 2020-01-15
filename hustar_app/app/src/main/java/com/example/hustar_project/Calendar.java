e1ec498d29b0253d797913d8e217235e08e80eea
qhrrl319 <qhrrl319@gmail.com>
2020-01-15 16:32:04 +0900

    APP 로그인+회원가입 기초 , calendaring

--- hustar_app/app/src/main/java/com/example/hustar_project/Calendar.java
+++ hustar_app/app/src/main/java/com/example/hustar_project/Calendar.java
@@ -1,14 +1,15 @@
 package com.example.hustar_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Calendar extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+
+public class Calendar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
     }
 }
--- hustar_app/app/src/main/java/com/example/hustar_project/MainActivity.java
+++ hustar_app/app/src/main/java/com/example/hustar_project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hustar_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calenderButton=(Button)findViewById(R.id.calendarButton);
+
+        calenderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calenderIntent=new Intent(MainActivity.this,Calendar.class);
+                MainActivity.this.startActivity(calenderIntent);
+            }
+        });
     }
 }
